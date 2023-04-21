0908d6051c7da35f20cf0c0cbc3c1c85765b3ec3
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-04-21 13:00:18 +0200

    update

--- app.js
+++ app.js
@@ -2,19 +2,39 @@ const myCanvas = document.getElementById('my-canvas');
 const ctx = myCanvas.getContext('2d');
 
 
-const point = {
-    x: 300,
-    y: 300
+let newGeneration = Generation.generateRandom(100, myCanvas.width, myCanvas.height);
+
+
+console.log(newGeneration)
+// const point = {
+//     x: 300,
+//     y: 300
+// }
+
+// setInterval(() => {
+   
+// },1)
+
+function step(timestamp){
+    
+    // console.log(timestamp)
+
+    newGeneration.draw(ctx)
+    if (newGeneration.isDead()){
+        ctx.fillStyle = 'rgba(0,0,0,0.1)'
+        ctx.fillRect(0,0, myCanvas.width, myCanvas.height)
+        newGeneration = Generation.generateRandom(100, myCanvas.width, myCanvas.height);
+    }
+    window.requestAnimationFrame(step)
 }
 
-
-
+window.requestAnimationFrame(step)
 // setInterval(() =>{
-    
+
 //     const randomX = (Math.random()*2) -1;
 //     const randomY= (Math.random()*2) -1;
 
-  
+
 //     ctx.fillStyle = 'red'
 //     ctx.fillRect(point.x, point.y, 2,2)
 //     point.x += randomX;
@@ -22,29 +42,21 @@ const point = {
 // },1)
 
 
-for(let i = 0; i < 100; i++){
-    const point = {
-        x: 300,
-        y: 300
-    }
-    
-    const red = Math.floor(Math.random()*256);
-    const green = Math.floor(Math.random()*256)
-    const blue = Math.floor(Math.random()*256)
-    const alpha = Math.random();
 
-    point.color= `rgba(${red},${green},${blue}, ${alpha})`
+
+// for (let i = 0; i < 100; i++) {
+//     // const point = {
+//     //     x: 300,
+//     //     y: 300
+//     // }
+
+//     const point = Point.generateRandom(myCanvas.width,myCanvas.height)
+
+//     setInterval(() => {
+//         point.draw(ctx);
+//         point.changePosition();
 
     
-setInterval(() =>{
     
-    const randomX = (Math.random()*2) -1;
-    const randomY= (Math.random()*2) -1;
-
-  
-    ctx.fillStyle = point.color
-    ctx.fillRect(point.x, point.y, 2,2)
-    point.x += randomX;
-    point.y += randomY;
-},1)
-}
+//     }, 1)
+// }
